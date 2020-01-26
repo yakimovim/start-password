dadd43d0ff997f7dc9b9c16a18ac323f0292de22
Ivan Yakimov <edlinsoft@gmail.com>
2020-01-26 17:58:46 +0300

    Initial implementation

---
+++ Starter/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows;
+
+namespace Starter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly byte[] _salt = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        private static readonly byte[] _iv = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnStartClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbPassword.Password))
+                return;
+
+            var encryptedApplicationFiles = Directory.GetFiles(
+                Environment.CurrentDirectory,
+                "*.exe.enc"
+                );
+
+            if(encryptedApplicationFiles.Length != 1)
+            {
+                MessageBox.Show("Unable to find single file to start");
+                Application.Current.Shutdown();
+            }
+
+            var encryptedApplicationPath = encryptedApplicationFiles[0];
+
+            var applicationPath = encryptedApplicationPath[0..^4];
+
+            if (File.Exists(applicationPath))
+            {
+                Process.Start(applicationPath);
+                Application.Current.Shutdown();
+            }
+
+            bStart.IsEnabled = false;
+
+            using var keyGenerator = new Rfc2898DeriveBytes(
+                tbPassword.Password, 
+                _salt,
+                1000,
+                HashAlgorithmName.SHA256);
+
+            using var encryptionAlgorithm = Rijndael.Create();
+
+            encryptionAlgorithm.BlockSize = 128;
+
+            var key = keyGenerator.GetBytes(encryptionAlgorithm.KeySize / 8);
+
+            var decryptor = encryptionAlgorithm.CreateDecryptor(key, _iv);
+
+            using var inputFileStream = new FileStream(
+                encryptedApplicationPath,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.Read
+                );
+
+            using var decryptionStream = new CryptoStream(inputFileStream, decryptor, CryptoStreamMode.Read);
+
+            using (var outputFileStream = new FileStream(
+                    applicationPath,
+                    FileMode.Create,
+                    FileAccess.Write
+                    ))
+            {
+                var buffer = new byte[1024];
+
+                while (true)
+                {
+                    int bytesRead = decryptionStream.Read(buffer, 0, 1024);
+                    if (bytesRead == 0)
+                        break;
+
+                    outputFileStream.Write(buffer, 0, bytesRead);
+                }
+            }
+
+            var startInfo = new ProcessStartInfo(applicationPath);
+
+            var process = Process.Start(startInfo);
+
+            Hide();
+
+            process.WaitForExit();
+
+            File.Delete(applicationPath);
+
+            Application.Current.Shutdown();
+
+            bStart.IsEnabled = true;
+        }
+    }
+}
---
+++ src/Encryptor/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows;
+
+namespace Encryptor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly byte[] _salt = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        private static readonly byte[] _iv = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnBrowseApplicationClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Application files (*.exe)|*.exe",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer),
+                Multiselect = false,
+                CheckPathExists = true,
+                CheckFileExists = true
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                tbApplicationPath.Text = dialog.FileName;
+            }
+        }
+
+        private void OnEncryptClick(object sender, RoutedEventArgs e)
+        {
+            if(File.Exists(tbApplicationPath.Text)
+                && !string.IsNullOrEmpty(tbPassword.Password))
+            {
+                bEncrypt.IsEnabled = false;
+
+                using var keyGenerator = new Rfc2898DeriveBytes(
+                    tbPassword.Password, 
+                    _salt,
+                    1000,
+                    HashAlgorithmName.SHA256);
+
+                using var encryptionAlgorithm = Rijndael.Create();
+
+                encryptionAlgorithm.BlockSize = 128;
+
+                var key = keyGenerator.GetBytes(encryptionAlgorithm.KeySize / 8);
+
+                var encryptor = encryptionAlgorithm.CreateEncryptor(key, _iv);
+
+                using var inputFileStream = new FileStream(
+                    tbApplicationPath.Text,
+                    FileMode.Open,
+                    FileAccess.Read,
+                    FileShare.Read
+                    );
+
+                using var outputFileStream = new FileStream(
+                    tbApplicationPath.Text + ".enc",
+                    FileMode.Create,
+                    FileAccess.Write
+                    );
+
+                using var ecnryptionStream = new CryptoStream(outputFileStream, encryptor, CryptoStreamMode.Write);
+
+                var buffer = new byte[1024];
+
+                while(true)
+                {
+                    int bytesRead = inputFileStream.Read(buffer, 0, 1024);
+                    if (bytesRead == 0)
+                        break;
+
+                    ecnryptionStream.Write(buffer, 0, bytesRead);
+                }
+
+                bEncrypt.IsEnabled = true;
+            }
+        }
+    }
+}
